5c35be8ee107f7761cf3033f8d68f120fea909cd
Josh Suson <josh.suson@gmail.com>
2022-02-10 14:27:47 -0500

    Initial commit

---
+++ app/routes/cognito.js
@@ -0,0 +1,36 @@
+import { json } from "remix";
+import Airtable from "airtable";
+
+export const action = async ({ request }) => {
+  if (request.method !== "POST") {
+    return json({ message: "Method not allowed" }, 405);
+  }
+
+  const payload = await request.json();
+  console.log(payload);
+
+  // const base = new Airtable({ apiKey: "keyTnMQzMxgghyQ4C" }).base(
+  //   "appGBMJd8O9jA8OAt"
+  // );
+
+  // base("Table 2").create(
+  //   [
+  //     {
+  //       fields: {
+  //         first_name: payload.firstName,
+  //         last_name: payload.lastName,
+  //         favorite_color: payload.favoriteColor,
+  //       },
+  //     },
+  //   ],
+  //   function (err, records) {
+  //     if (err) {
+  //       console.error(err);
+  //       return;
+  //     }
+  //     records.forEach((record) => console.log(record.getId()));
+  //   }
+  // );
+
+  return json({ success: true }, 200);
+};
