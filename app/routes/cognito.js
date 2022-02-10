14b8b961e952f509f043e28743255f09e753b0e2
Josh Suson <josh.suson@gmail.com>
2022-02-10 14:48:04 -0500

    trying to fix problems

--- app/routes/cognito.js
+++ app/routes/cognito.js
@@ -9,28 +9,28 @@ export const action = async ({ request }) => {
   const payload = await request.json();
   console.log(payload);
 
-  // const base = new Airtable({ apiKey: "keyTnMQzMxgghyQ4C" }).base(
-  //   "appGBMJd8O9jA8OAt"
-  // );
+  const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+    "appGBMJd8O9jA8OAt"
+  );
 
-  // base("Table 2").create(
-  //   [
-  //     {
-  //       fields: {
-  //         first_name: payload.firstName,
-  //         last_name: payload.lastName,
-  //         favorite_color: payload.favoriteColor,
-  //       },
-  //     },
-  //   ],
-  //   function (err, records) {
-  //     if (err) {
-  //       console.error(err);
-  //       return;
-  //     }
-  //     records.forEach((record) => console.log(record.getId()));
-  //   }
-  // );
+  base("Table 2").create(
+    [
+      {
+        fields: {
+          first_name: payload.firstName,
+          last_name: payload.lastName,
+          favorite_color: payload.favoriteColor,
+        },
+      },
+    ],
+    function (err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach((record) => console.log(record.getId()));
+    }
+  );
 
   return json({ success: true }, 200);
 };
